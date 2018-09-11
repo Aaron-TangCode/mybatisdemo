28b84029f1a0ee8cb57494c55897a573064dff3f
Aaron <1025741620@qq.com>
2018-09-11 16:49:58 +0800

    test33333

--- Test2/src/Test1.java
+++ Test2/src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("123123123");
         System.out.println("狗来的，愤怒");
+        System.out.println("狗来的，愤怒垃圾");
     }
 }
