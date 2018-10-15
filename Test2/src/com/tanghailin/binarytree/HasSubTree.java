fb4e71a17f35e4c6deb4847c74342c9daad23633
Aaron <1025741620@qq.com>
2018-10-15 21:50:22 +0800

    test1

--- Test2/src/com/tanghailin/binarytree/HasSubTree.java
+++ Test2/src/com/tanghailin/binarytree/HasSubTree.java
@@ -15,50 +15,43 @@ public class HasSubTree {
             this.val = val;
         }
     }
-    public static boolean hasSubTree(TreeNode root1,TreeNode root2){
-        //如果AB其中一个为null,那就返回false
-        if (root1==null){
+    public static boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if (root2==null) {        //空树不是任意一个树的子结构
             return false;
         }
-        if (root2==null){
+        if (root1==null) {        //如果A为空，那肯定返回false
             return false;
         }
-        if (root1.val==root2.val){
-            //如果AB的节点值相等，如果B没子树，那就直接返回true；
-            if (root2.left==null&&root2.right==null){
+        if(root2.val==root1.val){        //A和B比较的根节点的值相同
+
+            if (root2.left==null&&root2.right==null) {        //比较的节点是B的尾节点，递归截止
                 return true;
             }
-            //如果AB的节点值相等，如果B有子树，那么继续比较子树
-
-            //B有左右节点
-            if ((root2.left!=null&&root2.right!=null)&&(root1.left!=null&&root1.right!=null)&&root1.left.val==root2.left.val&&root1.right.val==root2.right.val){
-                return hasSubTree(root1.left,root2.left)&&hasSubTree(root1.right,root2.right);
-            }else if(root2.left!=null&&root1.left!=null&&root1.left.val==root2.left.val){
-                //B只有左子树
-                return hasSubTree(root1.left,root2.left);
-            }else if(root2.right!=null&&root1.right!=null&&root2.right.val==root1.right.val){
-                //B只有右子树
-                return hasSubTree(root1.right,root2.right);
-            }else{
-                //如果根节点不相等，那么就比较子节点
-                if(root1.left!=null&&root1.right!=null){
-                    return hasSubTree(root1.left,root2)||hasSubTree(root1.right,root2);
+            //下面三种比较的是比较的节点完全相同的情况
+            if ((root2.left!=null&&root2.right!=null)&&(root1.left!=null&&root1.right!=null)&&root2.left.val==root1.left.val&&root2.right.val==root1.right.val) {
+                return HasSubtree(root1.left,root2.left)&& HasSubtree(root1.right,root2.right);
+            }else if ((root2.left!=null&&root2.right==null)&&(root1.left!=null&&root1.right==null)&&root2.left.val==root1.left.val) {
+                return HasSubtree(root1.left,root2.left);
+            }else if ((root2.left==null&&root2.right!=null)&&(root1.left==null&&root1.right!=null)&&root2.right.val==root1.right.val) {
+                return HasSubtree(root1.right,root2.right);
+            }else{        //比较的节点不同，A向左右子节点移动一个再比较
+                if (root1.left!=null&&root1.right!=null) {
+                    return  HasSubtree(root1.left,root2)|| HasSubtree(root1.right,root2);
+                }else if(root1.left==null&&root1.right!=null){
+                    return  HasSubtree(root1.right,root2);
                 }else if(root1.left!=null&&root1.right==null){
-                    return hasSubTree(root1.left,root2);
-                }else if(root1.left ==null&&root1.right!=null){
-                    return hasSubTree(root1.right,root2);
-                }else{
+                    return  HasSubtree(root1.left,root2);
+                }else {
                     return false;
                 }
             }
-            //如果根节点的值不同，直接向左右节点移动比较
+            //比较的根节点的值不相同，直接向左右子节点滑动
         }else if(root1.left!=null&&root1.right==null){
-            return hasSubTree(root1.left,root2);
-        }else if(root1.left==null&&root1.right!=null) {
-            return hasSubTree(root1.right, root2);
+            return HasSubtree(root1.left,root2);
+        }else if(root1.left==null&&root1.right!=null){
+            return HasSubtree(root1.right,root2);
         }else{
             return false;
         }
-
     }
 }
